61254d146a6525f64007cab7c8a841092cf8a4ea
GustaLVS <168142217+GustaLVS@users.noreply.github.com>
2025-12-02 17:55:57 -0300

    navbar responsiva

--- script.js
+++ script.js
@@ -26,3 +26,12 @@ items.forEach((item) => {
         content.style.maxHeight = content.scrollHeight + "px";
     }
 });
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle("active");
+        navLinks.classList.toggle("active");
+    });
+
+
